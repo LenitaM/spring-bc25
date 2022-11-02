3b3eb16a230632c9016f95355c2b084008738e9d
LenitaM <lenitamaciel.ti@gmail.com>
2022-11-02 01:44:48 -0300

    Estrutura base do projeto

---
+++ src/main/java/org/soulcodeacademy/helpr/HelprApplication.java
@@ -0,0 +1,18 @@
+package org.soulcodeacademy.helpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*AULA DIA 31 DE OUT DE 2022
+ *
+ * ARQUIVO PRINCIPAL ONDE VAI RODAR O PROJETO
+ * */
+
+@SpringBootApplication
+public class HelprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelprApplication.class, args);
+	}
+
+}
